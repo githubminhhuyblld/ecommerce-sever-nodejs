0ec6f5bf1588ceb1d6ed01d1ed6d2fa9acfd625c
Nguyen Minh Huy <91535034+nhokkoihp01@users.noreply.github.com>
2023-07-09 11:42:59 +0700

    pagination ++

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -35,11 +35,24 @@ const productController = {
     },
     getAllProducts: async (req, res) => {
         try {
-            const products = await Product.find().populate({
-                path:"shop",
-                select:removeCommonFiled
+            const {page, limit} = req.query;
+
+            const options = {
+                page: parseInt(page, 10) || 1,
+                limit: parseInt(limit, 10) || 10,
+                populate: {
+                    path: "shop",
+                    select: removeCommonFiled,
+                },
+            };
+            const {docs, totalDocs, totalPages} = await Product.paginate({}, options);
+
+            res.status(200).json({
+                products: docs,
+                totalItems: totalDocs,
+                totalPages: totalPages,
+                currentPage: options.page,
             });
-            res.status(200).json(products)
         } catch (err) {
             res.status(500).json(err)
         }
@@ -92,28 +105,41 @@ const productController = {
     },
     filterProduct: async (req, res) => {
         try {
-            const {name, minPrice, maxPrice} = req.query;
-
+            const {name, minPrice, maxPrice, page, limit} = req.query;
             const conditions = {};
 
             switch (true) {
                 case Boolean(name):
-                    conditions.name = { $regex: new RegExp(name, 'i') };
+                    conditions.name = {$regex: new RegExp(name, 'i')};
                     break;
                 case Boolean(minPrice) && Boolean(maxPrice):
-                    conditions.newPrice = { $gte: minPrice, $lte: maxPrice };
+                    conditions.newPrice = {$gte: minPrice, $lte: maxPrice};
                     break;
                 case Boolean(minPrice):
-                    conditions.newPrice = { $gte: minPrice };
+                    conditions.newPrice = {$gte: minPrice};
                     break;
                 case Boolean(maxPrice):
-                    conditions.newPrice = { $lte: maxPrice };
+                    conditions.newPrice = {$lte: maxPrice};
                     break;
             }
+            const pageNumber = parseInt(page, 10) || 1;
+            const limitNumber = parseInt(limit, 10) || 10;
 
-            const products = await Product.find(conditions);
+            const totalProducts = await Product.countDocuments(conditions);
+            const totalPages = Math.ceil(totalProducts / limitNumber);
 
-            res.status(200).json({products});
+            const products = await Product.find(conditions)
+                .skip((pageNumber - 1) * limitNumber)
+                .limit(limitNumber);
+
+            res.status(200).json({
+                products,
+                totalItems: totalProducts,
+                totalPages,
+                currentPage: pageNumber
+            });
+
+
         } catch (error) {
             res.status(500).json({error: error.message});
         }
--- src/models/product.js
+++ src/models/product.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
 
 const productVariantSchema = new mongoose.Schema({
     variantName: {
@@ -84,4 +85,5 @@ const productSchema = new mongoose.Schema({
 
 }, {timestamps: true});
 
+productSchema.plugin(mongoosePaginate);
 module.exports = mongoose.model("Product", productSchema);
